11f400babb08411c63a851104d036a86959ad084
love77y <79936438+love77y@users.noreply.github.com>
2021-03-06 07:14:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+
+  TG_BOT_TOKEN = "1510919618:AAEA1iKruQ1LBPNp8SbiEdCeHdJIhe7DjPk"
+
+  APP_ID = 3479987
+
+  API_HASH = "2a128e8495ebfe8a5e0b3200afd80711”
+
+  AUTH_CHANNEL = -1001318029430
