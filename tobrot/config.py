3773ed62f17cf413b387ead39f30ce47f6a7433f
love77y <79936438+love77y@users.noreply.github.com>
2021-03-06 06:59:20 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,26 @@
+from tobrot.sample_config import Config
+class Config(Config):
+
+  TG_BOT_TOKEN = ""
+
+  APP_ID = 3479987
+
+  API_HASH = "2a128e8495ebfe8a5e0b3200afd80711”
+
+  AUTH_CHANNEL = -1001318029430
+
+  
+
+  YTDL_COMMAND = 'ytdl@Leechgroup_bot
+
+
+
+
+
+
+
+
+
+
+
+  
